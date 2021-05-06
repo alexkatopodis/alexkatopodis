6661b6a2553e3632013eb96ee504333f1e601a75
Alex Katopodis <alexkatopodis@live.com>
2021-05-06 16:59:01 -0300

    Criado filtro de pesquisas mês e ano

--- minha-carteira/src/pages/Dashboard/index.tsx
+++ minha-carteira/src/pages/Dashboard/index.tsx
@@ -19,7 +19,7 @@ const Dashboard : React.FC = () => {
     return (
         <Container>
             <ContentHeader title='DashBoard' lineColor='#f7931b'>
-                <SelectInput options ={options}/>
+                <SelectInput options ={options} onChange={() => {}}/>
             </ContentHeader>
         </Container>
     );
